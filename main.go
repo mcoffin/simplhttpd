9fb63cc562c51dd821c5a0a74e6a951ff8655c50
Matt Coffin <mcoffin13@gmail.com>
2014-09-10 11:59:29 -0600

    Chdir to 'root' on startup
    Fix #1

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"flag"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fileRoot := flag.String("root", ".", "file system root")
 	bind := flag.String("http", ":8080", "http binding")
 	flag.Parse()
+
+	err := os.Chdir(*fileRoot)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	handler := http.FileServer(http.Dir(*fileRoot))
 	http.ListenAndServe(*bind, handler)
